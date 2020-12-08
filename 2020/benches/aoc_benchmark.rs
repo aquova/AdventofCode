2ac5b051b8282a8ca5502c06f82af0a9cd676e03
aquova <abrick912@gmail.com>
2020-12-07 23:29:46 -0600

    Finished 2020 day 8

--- 2020/benches/aoc_benchmark.rs
+++ 2020/benches/aoc_benchmark.rs
@@ -2,8 +2,8 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use aoc_2020::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("aoc_7p1", |b| b.iter(|| day7::day7p1()));
-    c.bench_function("aoc_7p2", |b| b.iter(|| day7::day7p2()));
+    c.bench_function("aoc_8p1", |b| b.iter(|| day8::day8p1()));
+    c.bench_function("aoc_8p2", |b| b.iter(|| day8::day8p2()));
 }
 
 criterion_group!(benches, criterion_benchmark);
---
+++ 2020/src/day8/mod.rs
@@ -0,0 +1,95 @@
+use crate::utils::read_lines;
+
+pub fn day8p1() -> Option<i32> {
+    let input = read_lines("src/day8/input.txt");
+    let mut acc = 0;
+    let mut pc = 0;
+    let mut visited: Vec<usize> = Vec::new();
+
+    loop {
+        if visited.contains(&pc) {
+            break;
+        } else {
+            visited.push(pc);
+        }
+        let line = &input[pc];
+        let words: Vec<&str> = line.split(" ").collect();
+        match words[0] {
+            "acc" => {
+                let diff: i32 = words[1].parse().unwrap();
+                acc += diff;
+                pc += 1;
+            },
+            "jmp" => {
+                let rel: isize = words[1].parse().unwrap();
+                let new_pc = (pc as isize) + rel;
+                pc = new_pc as usize;
+            },
+            _ => {
+                // Includes nop
+                pc += 1;
+            }
+        }
+    }
+
+    Some(acc)
+}
+
+pub fn day8p2() -> Option<i32> {
+    let input = read_lines("src/day8/input.txt");
+
+    let mut modified_pc = 0;
+    while modified_pc < input.len() {
+        let mut acc = 0;
+        let mut pc = 0;
+        let mut visited: Vec<usize> = Vec::new();
+
+        loop {
+            if pc >= input.len() {
+                return Some(acc);
+            }
+
+            if visited.contains(&pc) {
+                break;
+            } else {
+                visited.push(pc);
+            }
+
+            let line = &input[pc];
+            let words: Vec<&str> = line.split(" ").collect();
+
+            let op = if pc == modified_pc {
+                if words[0] == "jmp" {
+                    "nop"
+                } else if words[0] == "nop" {
+                    "jmp"
+                } else {
+                    words[0]
+                }
+            } else {
+                words[0]
+            };
+
+            match op {
+                "acc" => {
+                    let diff: i32 = words[1].parse().unwrap();
+                    acc += diff;
+                    pc += 1;
+                },
+                "jmp" => {
+                    let rel: isize = words[1].parse().unwrap();
+                    let new_pc = (pc as isize) + rel;
+                    pc = new_pc as usize;
+                },
+                _ => {
+                    // Includes nop
+                    pc += 1;
+                }
+            }
+        }
+
+        modified_pc += 1;
+    }
+
+    None
+}
